0eb5c3ff25d8e2a9e18607da2e98ed69a63c4eb0
James Pederson <james@jpederson.com>
2015-09-21 12:03:34 -0500

    Initial commit.

---
+++ widget.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Twitter Aggregator Widget
+Plugin URI: 
+Description: A widget to pull multiple twitter feeds into a local cache and provide a widget that displays them.
+Author: James Pederson
+Version: 0.0.1
+*/
+
+
+
+?>

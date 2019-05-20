2a0c322d0deb6f20f02bedb9596f32f7e7eea1a1
James Pederson <james@jpederson.com>
2019-05-20 15:57:10 -0500

    Add some comments, create a new `widget_unstyled()` method for the main class, add wordpress shortcode and settings interface initial code.

--- twitterAggregator.php
+++ twitterAggregator.php
@@ -1,7 +1,10 @@
 <?php
 
 
-// the twitter aggregator class
+/************************************
+  twitter aggregator
+ ***********************************/
+
 class twitterAggregator {
 
     // our options array
@@ -212,6 +215,20 @@ class twitterAggregator {
     // get the widget code
     public function widget() {
 
+        $html = '<style>' . file_get_contents( __DIR__ . '/widget.css' ) . '</style>';
+
+        // get the timelines
+        $html .= $this->widget_unstyled();
+
+        // return widget with styles
+        return $html;
+    }
+
+
+
+    // get the widget code
+    public function widget_unstyled() {
+
         // get the timelines
         $this->fetch();
 
@@ -249,11 +266,6 @@ class twitterAggregator {
 
     // display the widget
     public function display() {
-        
-        // fetch the and display widget styles
-        print '<style>' . file_get_contents( __DIR__ . '/widget.css' ) . '</style>';
-
-        // display the widget
         print $this->widget();
     }
 
@@ -261,9 +273,7 @@ class twitterAggregator {
 
     // display the widget
     public function display_unstyled() {
-        
-        // display the widget
-        print $this->widget();
+        print $this->widget_unstyled();
     }
 
 
@@ -318,3 +328,111 @@ class twitterAggregator {
     }
 }
 
+
+/************************************
+  wordpress functionality
+ ***********************************/
+
+// check if we're inside wordpress
+if ( function_exists( 'add_shortcode' ) ) {
+
+    // twitter aggregator options for the wordpress settings page and the shortcode
+    $ta_options = array( 
+        'ta_consumer_key', 
+        'ta_consumer_secret', 
+        'ta_oauth_access_token', 
+        'ta_oauth_access_token_secret', 
+        'ta_usenames'
+    );
+
+
+    /************************************
+      shortcode [twitter-aggregator]
+     ***********************************/
+    function twitter_aggregator_shortcode( $atts ) {
+
+        // default parameters
+        $a = shortcode_atts( array(
+            'consumer_key' => get_option( "ta_consumer_key", "" ),
+            'consumer_secret' => get_option( "ta_consumer_secret", "" ),
+            'oauth_access_token' => get_option( "ta_oauth_access_token", "" ),
+            'oauth_access_token_secret' => get_option( "ta_oauth_access_token_secret", "" ),
+            'usernames' => get_option( "ta_usenames", "reuters,ap,propublica" ),
+            'update_interval' => 10,
+            'styles' => true,
+            'count' => 10,
+            'exclude_replies' => true,
+            'include_rts' => true
+        ), $atts );
+
+
+        // generate an aggregator object
+        $ta = new twitterAggregator( $a );
+
+        if ( $a['styles'] ) {
+            // display the widget
+            return $ta->widget();
+        } else {
+            // display the widget
+            return $ta->widget_unstyled();
+        }
+    }
+
+    // register our function as a shortcode
+    add_shortcode( 'twitter-aggregator', 'twitter_aggregator_shortcode' );
+
+
+
+    /************************************
+      settings interface
+     ***********************************/
+
+    // register the settings for the first time.
+    function ta_register_settings() {
+        global $ta_options;
+
+        // loop through the options and add them.
+        foreach ( $ta_options as $opt ) {
+            add_option( $opt, '');
+            register_setting( 'ta_options_group', $opt );
+        }
+    }
+    add_action( 'admin_init', 'ta_register_settings' );
+
+
+    // register the options page in the admin menu
+    function ta_register_options_page() {
+        add_options_page('Twitter Aggregator Settings', 'Twitter Aggregator', 'manage_options', 'ta', 'ta_options_page');
+    }
+    add_action('admin_menu', 'ta_register_options_page');
+
+
+    // the actual page output function
+    function ta_options_page() {
+        global $ta_options;
+        
+        ?>
+        <h2>Twitter Aggregator Settings</h2>
+        <form method="post" action="options.php">
+        <?php
+
+        // settings field group
+        settings_fields( 'ta_options_group' );
+
+        // loop through the options and output fields.
+        foreach ( $ta_options as $opt ) {
+            ?>
+            <p><label for="<?php print $opt ?>">Label</label><br>
+                <input type="text" id="<?php print $opt; ?>" name="<?php print $opt; ?>" value="<?php echo get_option( $opt ); ?>" /></p>
+            <?php
+        }
+
+        // add the submit button.
+        submit_button();
+
+        ?>
+        </form>
+        <?php
+    }
+
+}
